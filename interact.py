7de2767f2bad468046e4e87db44f4fff42c336e7
Crowded-dorm <xx60507920530@gmail.com>
2023-05-03 09:00:09 +0000

    Update interact.py

--- interact.py
+++ interact.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 def selectSourceImage(img_arr: list):
     """
     """
+    print("Select ...")
     # Show all images (plt)
     id = input('Enter the source image id: ')
     # Return the image with the id
