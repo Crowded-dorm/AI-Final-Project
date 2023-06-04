ad56f83bc57f1d81144aa1ea275e6ca5719ac72d
Oceanic <51773435+Su-Terry@users.noreply.github.com>
2023-06-04 12:29:46 +0000

    bug fix

--- mergetoGIF.py
+++ mergetoGIF.py
@@ -1,7 +1,6 @@
 import glob
 import imageio
 import cv2
-import json
 import os
 import argparse
 
@@ -9,20 +8,15 @@ def isimage(path):
     ext = os.path.splitext(path)[-1].lower()
     return ext == ".png" or ext == ".jpg" or ext == ".jpeg"
 
-def main(input_dir, config):
+def main(input_dir):
     img_paths = sorted(list(filter(isimage, glob.glob(f"{input_dir}/*"))))
     frames = [cv2.imread(im)[...,::-1] for im in img_paths]
-    with open(config, 'r') as f:
-        time_specs = json.load(f)
-    seq = list(time_specs.keys())[0]
-    spec = time_specs[seq]
-    fps = spec["fps"]
-    imageio.mimsave(f'{input_dir}/result.gif', frames, duration=1000/fps)
+    imageio.mimsave(f'{input_dir}/result.gif', frames)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', default='custom_dataset/results/motion_sculpture', help='input directory')
-    parser.add_argument('-c', '--config', default='custom_dataset/custom.json', help='config file')
+    #parser.add_argument('-c', '--config', default='custom_dataset/custom.json', help='config file')
     args = parser.parse_args()
     
-    main(args.input, args.config)
+    main(args.input)
